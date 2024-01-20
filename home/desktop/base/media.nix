e54cb49e0a1a1cac3cd0821673732aeae6f6981a
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 19:00:16 +0300

    syntax fix

--- home/desktop/base/media.nix
+++ home/desktop/base/media.nix
@@ -24,11 +24,10 @@
   ];
 
   # https://github.com/catppuccin/cava
-  xdg.configFile."cava/config".text =
-    ''
-      # custom cava config
-    ''
-    # + builtins.readFile "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-cava}/mocha.cava";
+  xdg.configFile."cava/config".text = ''
+    # custom cava config
+  '';
+  # + builtins.readFile "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-cava}/mocha.cava";
 
   programs = {
     mpv = {
