0e0a31f7ceee892f80dbb471362d01f7de019e52
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 23:04:44 +0300

    removing config symlink temporarily

--- home/base/server/default.nix
+++ home/base/server/default.nix
@@ -9,22 +9,22 @@ in {
   imports = mylib.scanPaths ./.;
 
   home = {
-    file.".config/nvim" = {
-      #source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim";
-      # source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim-nixos";
-      source = "${config_sym_dir}/nvim-nixos";
-      # source = mylib.symlinkConfig "/nvim-nixos";
-      recursive = true;
-      # target = ".config/nvim";
-    };
-    #file.".config/tmux" = {
-    # source = "${config_sym_dir}/tmux";
-    #  recursive = true;
-    #};
-    file.".config/hypr" = {
-      source = "${config_sym_dir}/hypr";
-      # source = mylib.symlinkConfig "/hypr";
-      recursive = true;
-    };
+    # file.".config/nvim" = {
+    #   #source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim";
+    #   # source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim-nixos";
+    #   source = "${config_sym_dir}/nvim-nixos";
+    #   # source = mylib.symlinkConfig "/nvim-nixos";
+    #   recursive = true;
+    #   # target = ".config/nvim";
+    # };
+    # #file.".config/tmux" = {
+    # # source = "${config_sym_dir}/tmux";
+    # #  recursive = true;
+    # #};
+    # file.".config/hypr" = {
+    #   source = "${config_sym_dir}/hypr";
+    #   # source = mylib.symlinkConfig "/hypr";
+    #   recursive = true;
+    # };
   };
 }
