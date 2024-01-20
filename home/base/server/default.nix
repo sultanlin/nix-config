edd75e321fb164f6caff35f107f356118821152c
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-21 06:08:16 +0800

    symlink config file

--- home/base/server/default.nix
+++ home/base/server/default.nix
@@ -9,22 +9,22 @@ in {
   imports = mylib.scanPaths ./.;
 
   home = {
-    #file.".config/nvim" = {
+    file.".config/nvim" = {
       #source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim";
       # source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim-nixos";
-    #  source = "${config_sym_dir}/nvim-nixos";
+      source = "${config_sym_dir}/nvim-nixos";
       # source = mylib.symlinkConfig "/nvim-nixos";
-    #  recursive = true;
+      recursive = true;
       # target = ".config/nvim";
-    #};
+    };
     #file.".config/tmux" = {
     # source = "${config_sym_dir}/tmux";
     #  recursive = true;
     #};
-    file.".config/hypr" = {
-      source = "${config_sym_dir}/hypr";
+    #file.".config/hypr" = {
+    #  source = "${config_sym_dir}/hypr";
       # source = mylib.symlinkConfig "/hypr";
-      recursive = true;
-    };
+    #  recursive = true;
+    #};
   };
 }
