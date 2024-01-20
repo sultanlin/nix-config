d56cc4a4062ad59915eb78acf4a5625f8bb18ae6
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 20:16:22 +0300

    removing confusing nixpkgs thing

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -1,15 +1,16 @@
 {
   lib,
-  nixpkgs,
+  # nixpkgs,
+  inputs,
   ...
 }: {
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = nixpkgs;
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
 
   # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
   # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
-  environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
   # https://github.com/NixOS/nix/issues/9574
   nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
 }
