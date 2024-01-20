e7dfc07f36343de20bb7b32502ea283dbc7b40b7
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 19:42:32 +0300

    removing confusing nixpkgs thing

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  pkgs,
+  nixpkgs,
   ...
 }: {
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = pkgs;
+  nix.registry.nixpkgs.flake = nixpkgs;
   nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
 
   # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
