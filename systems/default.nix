3bfd5deb8af5606ac4b95b407599815529373ab4
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 19:04:55 +0300

    adding dotfilesConfig to specialArgs

--- systems/default.nix
+++ systems/default.nix
@@ -9,7 +9,7 @@
 
   specialArgsForSystem = system:
     {
-      inherit (constants) username userfullname useremail;
+      inherit (constants) username userfullname useremail dotfilesConfig;
       inherit mylib;
       # use unstable branch for some packages to get the latest updates
       pkgs-stable = import inputs.nixpkgs-stable {
