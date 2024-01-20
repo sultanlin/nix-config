9746ae25f65c6c4d54d6c6064609bb3836231751
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 18:33:40 +0300

    syntax error, replacing manual imports with mylib imports

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,10 +1,14 @@
-{
-  imports = [
-    ./fonts.nix
-    ./graphic.nix
-    ./misc.nix
-    ./peripherals.nix
-    ./security.nix
-    ./visualisation.nix
-  ];
+{mylib, ...}: {
+  imports = mylib.scanPaths ./.;
 }
+# {
+#   imports = [
+#     ./fonts.nix
+#     ./graphic.nix
+#     ./misc.nix
+#     ./peripherals.nix
+#     ./security.nix
+#     # ./visualisation.nix
+#   ];
+# }
+
