6ebbd09fec85bd0659763fa9b55267e16c20f1ec
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 18:11:38 +0300

    second commit

--- systems/vars.nix
+++ systems/vars.nix
@@ -1,26 +1,37 @@
 let
   desktop_base_modules = {
     nixos-modules = [
-      ../secrets/nixos.nix
-      ../modules/nixos/desktop.nix
+      # ../modules/nixos/desktop.nix
+      ../modules/hyprland.nix
     ];
     home-module.imports = [
-      ../home/linux/desktop.nix
+      ../home/desktop.nix
     ];
   };
 in {
-  idol_ai_modules_hyprland = {
+  laptop_modules = {
     nixos-modules =
       [
-        ../hosts/idols/ai
-        {modules.desktop.wayland.enable = true;}
+        ../hosts/laptop
+        # {modules.desktop.wayland.enable = true;}
       ]
       ++ desktop_base_modules.nixos-modules;
     home-module.imports =
       [
-        ../hosts/idols/ai/home.nix
-        {modules.desktop.hyprland.enable = true;}
+        ../hosts/laptop/home.nix
+        # {modules.desktop.hyprland.enable = true;}
       ]
       ++ desktop_base_modules.home-module.imports;
+  };
+  vm_modules = {
+    nixos-modules = [
+      ../hosts/vm
+      ../modules/server.nix
+      # ../modules/nixos/proxmox-hardware-configuration.nix
+    ];
+    home-module.imports = [
+      ../hosts/vm/home.nix
+      ../home/server.nix
+    ];
   };
 }
