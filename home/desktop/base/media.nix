ef9a7e2af7dcac004909739ec3bbb20dee9cc069
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-21 10:43:01 +0800

    ?

--- home/desktop/base/media.nix
+++ home/desktop/base/media.nix
@@ -32,8 +32,8 @@
   programs = {
     mpv = {
       enable = true;
-      defaultProfiles = ["gpu-hq"];
-      scripts = [pkgs.mpvScripts.mpris];
+      # defaultProfiles = ["gpu-hq"];
+      # scripts = [pkgs.mpvScripts.mpris];
     };
   };
 
