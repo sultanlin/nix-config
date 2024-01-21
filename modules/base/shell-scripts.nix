852ee9ac03e2734119f0d165f6effba07b21fd24
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-22 04:28:00 +0800

    setting up direnv

--- modules/base/shell-scripts.nix
+++ modules/base/shell-scripts.nix
@@ -3,14 +3,12 @@
   pkgs,
   nixosPath,
   ...
-}: let
-  shellPath = script: (import ../../scripts/${script} {inherit pkgs;});
-in {
-  environment.systemPackages = [
-    shellPath
-    ./direnv-create.nix
-    shellPath
-    ./direnv-add.nix
+}: {
+  environment.systemPackages = let
+    shellPath = script: (import ../../scripts/${script} {inherit pkgs;});
+  in [
+    (shellPath ./direnv-create.nix)
+    (shellPath ./direnv-add.nix)
     # (import shellPath ./direnv-create.nix {inherit pkgs;})
     # (import shellPath ./direnv-add.nix {inherit pkgs;})
     # (import /. + builtins.toPath "${nixosPath}/scripts/direnv-create.nix" {inherit pkgs;})
