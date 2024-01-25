8af544297ea3578254777688e0133bbdd8054f6f
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-25 03:42:42 +0300

    saving zsh settings

--- home/base/server/zsh.nix
+++ home/base/server/zsh.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  dotfilesConfig,
   ...
 }: {
   programs = {
@@ -124,4 +125,15 @@
     ripgrep
     tldr
   ];
+  # home.file.""
+  home.file.".config/zsh/.p10k.zsh" = {
+    # /home/sultan/nix-config/dotfiles/config/zsh/.p10k.zsh
+    source = "${dotfilesConfig}/zsh/.p10k.zsh";
+    # source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim-nixos";
+    # source = "${config_sym_dir}/nvim-nixos";
+    # source = "${config_sym_dir}/nvim";
+    # source = mylib.symlinkConfig "/nvim-nixos";
+    recursive = true;
+    # target = ".config/nvim";
+  };
 }
