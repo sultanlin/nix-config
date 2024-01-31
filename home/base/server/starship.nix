fbce5bbc14300519692d878d7639479c1111c0d7
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-31 19:14:32 +0300

    testing starship.toml symlink

--- home/base/server/starship.nix
+++ home/base/server/starship.nix
@@ -1,6 +1,8 @@
 {
   catppuccin-starship,
   dotfilesConfigPath,
+  dotfilesConfig,
+  config,
   ...
 }: {
   programs.starship = {
@@ -13,28 +15,30 @@
 
     enableTransience = true;
 
-    settings =
-      {
-        character = {
-          success_symbol = "[›](bold green)";
-          error_symbol = "[›](bold red)";
-        };
-        aws = {
-          symbol = "🅰 ";
-        };
-        gcloud = {
-          # do not show the account/project's info
-          # to avoid the leak of sensitive information when sharing the terminal
-          format = "on [$symbol$active(\($region\))]($style) ";
-          symbol = "🅶 ️";
-        };
-
-        palette = "catppuccin_mocha";
-
-        add_newline = false;
-        line_break.disabled = true;
-      }
-      // builtins.fromTOML (builtins.readFile "${dotfilesConfigPath}/starship/starship.toml")
-      // builtins.fromTOML (builtins.readFile "${catppuccin-starship}/palettes/mocha.toml");
+    # settings =
+    #   {
+    #     character = {
+    #       success_symbol = "[›](bold green)";
+    #       error_symbol = "[›](bold red)";
+    #     };
+    #     aws = {
+    #       symbol = "🅰 ";
+    #     };
+    #     gcloud = {
+    #       # do not show the account/project's info
+    #       # to avoid the leak of sensitive information when sharing the terminal
+    #       format = "on [$symbol$active(\($region\))]($style) ";
+    #       symbol = "🅶 ️";
+    #     };
+    #
+    #     palette = "catppuccin_mocha";
+    #
+    #     add_newline = false;
+    #     line_break.disabled = true;
+    #   }
+    #   // builtins.fromTOML (builtins.readFile "${dotfilesConfigPath}/starship/starship.toml")
+    #   // builtins.fromTOML (builtins.readFile "${catppuccin-starship}/palettes/mocha.toml");
   };
+  # config_sym_dir = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}";
+  home.file.".config/starship.toml" = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/starship/starship.toml";
 }
