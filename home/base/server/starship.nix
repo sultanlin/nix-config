98ef7378ec4c7fff4e993eea003f5963cbe0df00
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-31 19:10:41 +0300

    organizing starship config

--- home/base/server/starship.nix
+++ home/base/server/starship.nix
@@ -34,7 +34,7 @@
         add_newline = false;
         line_break.disabled = true;
       }
-      // builtins.fromTOML (builtins.readFile "${dotfilesConfigPath}/starship.toml")
+      // builtins.fromTOML (builtins.readFile "${dotfilesConfigPath}/starship/starship.toml")
       // builtins.fromTOML (builtins.readFile "${catppuccin-starship}/palettes/mocha.toml");
   };
 }
