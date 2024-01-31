0a7e7668be61cb3249fb28e687415593b5642bef
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-31 19:18:43 +0300

    testing starship.toml symlink

--- home/base/server/starship.nix
+++ home/base/server/starship.nix
@@ -15,27 +15,26 @@
 
     enableTransience = true;
 
-    # settings =
-    #   {
-    #     character = {
-    #       success_symbol = "[›](bold green)";
-    #       error_symbol = "[›](bold red)";
-    #     };
-    #     aws = {
-    #       symbol = "🅰 ";
-    #     };
-    #     gcloud = {
-    #       # do not show the account/project's info
-    #       # to avoid the leak of sensitive information when sharing the terminal
-    #       format = "on [$symbol$active(\($region\))]($style) ";
-    #       symbol = "🅶 ️";
-    #     };
-    #
-    #     palette = "catppuccin_mocha";
-    #
-    #     add_newline = false;
-    #     line_break.disabled = true;
-    #   }
+    settings = {
+      character = {
+        success_symbol = "[›](bold green)";
+        error_symbol = "[›](bold red)";
+      };
+      aws = {
+        symbol = "🅰 ";
+      };
+      gcloud = {
+        # do not show the account/project's info
+        # to avoid the leak of sensitive information when sharing the terminal
+        format = "on [$symbol$active(\($region\))]($style) ";
+        symbol = "🅶 ️";
+      };
+
+      # palette = "catppuccin_mocha";
+
+      add_newline = false;
+      line_break.disabled = true;
+    };
     #   // builtins.fromTOML (builtins.readFile "${dotfilesConfigPath}/starship/starship.toml")
     #   // builtins.fromTOML (builtins.readFile "${catppuccin-starship}/palettes/mocha.toml");
   };
