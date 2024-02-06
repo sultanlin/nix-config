4d8d0453f9ffe5eea9191a416fb92f9ab2d23446
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-02-06 11:59:25 +0300

    add vim-tmux-navigator

--- home/base/server/tmux.nix
+++ home/base/server/tmux.nix
@@ -2,17 +2,18 @@
   config,
   pkgs,
   ...
-}: 
-{
+}: {
   programs.tmux = {
     enable = true;
-    shell = "${pkgs.zsh}/bin/zsh";
+    # shell = "${pkgs.zsh}/bin/zsh";
 
     # extraConfig =  builtins.readFile ./tmux.conf;
     # keyMode = "vi";  # default is emacs
 
-    baseIndex = 1; # start index from 1
-    escapeTime = 0; # do not wait for escape key
-    terminal = "xterm-256color";
+    # baseIndex = 1; # start index from 1
+    # escapeTime = 0; # do not wait for escape key
+    # terminal = "xterm-256color";
+
+    plugins = with pkgs.tmuxPlugins; [vim-tmux-navigator];
   };
 }
