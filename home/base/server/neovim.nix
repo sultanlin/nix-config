5f8ff2c2c94ed786e399939c43b90df3f5f6c7cd
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-02-08 20:11:19 +0300

    trying neovim nightly

--- home/base/server/neovim.nix
+++ home/base/server/neovim.nix
@@ -32,7 +32,7 @@
       # package = neovim-nightly.packages."${pkgs.system}".default.override (old: old // {inherit (pkgs) libvterm-neovim;});
       # package = neovim-nightly.packages."${pkgs.system}".neovim;
       # package = pkgs.neovim-nightly.overrideAttrs (_: {CFLAGS = "-O3";});
-      package = pkgs.neovim-nightly;
+      package = pkgs.neovim-nightly.neovim;
 
       viAlias = true;
       vimAlias = true;
