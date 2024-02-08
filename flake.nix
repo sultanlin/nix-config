7efaf94ab3f154cdf49805fb860b1565ae941680
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-02-08 20:43:35 +0300

    trying neovim nightly

--- flake.nix
+++ flake.nix
@@ -145,7 +145,7 @@
     neovim-nightly = {
       # https://github.com/neovim/neovim/blob/master/BUILD.md
       url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
       # url = "github:neovim/neovim?dir=contrib";
       # inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
