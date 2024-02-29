ccfdabd44b9fa4d7f4bce26cba4962a06bc3b8d4
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-01 02:22:59 +0300

    resolving config and plugins

--- home/base/server/tmux.nix
+++ home/base/server/tmux.nix
@@ -9,31 +9,7 @@
     # shell = "${pkgs.zsh}/bin/zsh";
 
     # extraConfig = builtins.readFile dotfilesConfigPath + /tmux/tmux.conf;
-    extraConfig = ''
-      source-file ~/.config/tmux/extras/tmux.conf
-      # set -g status-position top       # macOS / darwin style
-      # bind R source-file ~/.config/tmux/tmux.conf
-
-      set-option -g terminal-overrides ',xterm-256color:RGB'
-      # set -g @catppuccin_window_left_separator ""
-      # set -g @catppuccin_window_right_separator " "
-      # set -g @catppuccin_window_middle_separator " █"
-      # set -g @catppuccin_window_number_position "right"
-      # set -g @catppuccin_window_default_fill "number"
-      # set -g @catppuccin_window_default_text "#W"
-      # set -g @catppuccin_window_current_fill "number"
-      # set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-      # set -g @catppuccin_status_modules_right "directory meetings date_time"
-      # set -g @catppuccin_status_modules_left "session"
-      # set -g @catppuccin_status_left_separator  " "
-      # set -g @catppuccin_status_right_separator " "
-      # set -g @catppuccin_status_right_separator_inverse "no"
-      # set -g @catppuccin_status_fill "icon"
-      # set -g @catppuccin_status_connect_separator "no"
-      # set -g @catppuccin_directory_text "#{b:pane_current_path}"
-      # set -g @catppuccin_meetings_text "#($HOME/.config/tmux/scripts/cal.sh)"
-      # set -g @catppuccin_date_time_text "%H:%M"
-    '';
+    extraConfig = ''source-file ~/.config/tmux/extras/tmux.conf'';
     keyMode = "vi"; # default is emacs
 
     baseIndex = 1; # start index from 1
@@ -48,14 +24,14 @@
 
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
-      # gruvbox
-      # catppuccin
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor 'mocha'
-        '';
-      }
+      gruvbox
+      catppuccin
+      # {
+      #   plugin = catppuccin;
+      #   extraConfig = ''
+      #     set -g @catppuccin_flavor 'mocha'
+      #   '';
+      # }
       battery
     ];
   };
