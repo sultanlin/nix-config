c3dc2aef386f6e7b0c234d40f095f5a8d4cc28a3
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-08 18:25:33 +0300

    moving home manager to standalone

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -10,25 +10,47 @@
 in
   nixpkgs.lib.nixosSystem {
     inherit system specialArgs;
-    modules =
+    # Home manager standalone
+    modules = [
       nixos-modules
-      ++ [
-        # nixos-generators.nixosModules.all-formats
-        # {
-        #   # formatConfigs.iso = {config, ...}: {};
-        #   formatConfigs.proxmox = {config, ...}: {
-        #     # custom proxmox's image name
-        #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
-        #   };
-        # }
-        #
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+      # nixos-generators.nixosModules.all-formats
+      # {
+      #   # formatConfigs.iso = {config, ...}: {};
+      #   formatConfigs.proxmox = {config, ...}: {
+      #     # custom proxmox's image name
+      #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
+      #   };
+      # }
+      #
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
 
-          home-manager.extraSpecialArgs = specialArgs;
-          home-manager.users."${username}" = home-module;
-        }
-      ];
+        home-manager.extraSpecialArgs = specialArgs;
+        home-manager.users."${username}" = home-module;
+      }
+    ];
+    # NixOS and home manager together
+    # modules =
+    #   nixos-modules
+    #   ++ [
+    #     # nixos-generators.nixosModules.all-formats
+    #     # {
+    #     #   # formatConfigs.iso = {config, ...}: {};
+    #     #   formatConfigs.proxmox = {config, ...}: {
+    #     #     # custom proxmox's image name
+    #     #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
+    #     #   };
+    #     # }
+    #     #
+    #     home-manager.nixosModules.home-manager
+    #     {
+    #       home-manager.useGlobalPkgs = true;
+    #       home-manager.useUserPackages = true;
+    #
+    #       home-manager.extraSpecialArgs = specialArgs;
+    #       home-manager.users."${username}" = home-module;
+    #     }
+    #   ];
   }
