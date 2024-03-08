438014cee3a34c0984074028db08593a0a03ee0e
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-08 19:06:38 +0300

    moving home manager to standalone

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -11,28 +11,28 @@ in
   nixpkgs.lib.nixosSystem {
     inherit system specialArgs;
     modules =
-      nixos-modules
-      ++ [
-        # nixos-generators.nixosModules.all-formats
-        # {
-        #   # formatConfigs.iso = {config, ...}: {};
-        #   formatConfigs.proxmox = {config, ...}: {
-        #     # custom proxmox's image name
-        #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
-        #   };
-        # }
-        #
+      nixos-modules;
+    # ++ [
+    # nixos-generators.nixosModules.all-formats
+    # {
+    #   # formatConfigs.iso = {config, ...}: {};
+    #   formatConfigs.proxmox = {config, ...}: {
+    #     # custom proxmox's image name
+    #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
+    #   };
+    # }
+    #
 
-        # Home manager as a NixOS module
-        home-manager.nixosModules.home-manager
-        # {
-        #   home-manager.useGlobalPkgs = true;
-        #   home-manager.useUserPackages = true;
-        #
-        #   home-manager.extraSpecialArgs = specialArgs;
-        #   home-manager.users."${username}" = home-module;
-        # }
-      ];
+    # Home manager as a NixOS module
+    # home-manager.nixosModules.home-manager
+    # {
+    #   home-manager.useGlobalPkgs = true;
+    #   home-manager.useUserPackages = true;
+    #
+    #   home-manager.extraSpecialArgs = specialArgs;
+    #   home-manager.users."${username}" = home-module;
+    # }
+    # ];
 
     # Home manager as a standalone
     # home-manager = {
