7541937b7dbfc5cf0b192b563f33e1f1aa183496
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-08 18:40:11 +0300

    moving home manager to standalone

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -10,47 +10,35 @@
 in
   nixpkgs.lib.nixosSystem {
     inherit system specialArgs;
-    # Home manager standalone
-    modules = [
+    modules =
       nixos-modules
-      # nixos-generators.nixosModules.all-formats
-      # {
-      #   # formatConfigs.iso = {config, ...}: {};
-      #   formatConfigs.proxmox = {config, ...}: {
-      #     # custom proxmox's image name
-      #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
-      #   };
-      # }
-      #
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
+      ++ [
+        # nixos-generators.nixosModules.all-formats
+        # {
+        #   # formatConfigs.iso = {config, ...}: {};
+        #   formatConfigs.proxmox = {config, ...}: {
+        #     # custom proxmox's image name
+        #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
+        #   };
+        # }
+        #
+        # Home manager as a NixOS module
+        home-manager.nixosModules.home-manager
+        # {
+        #   home-manager.useGlobalPkgs = true;
+        #   home-manager.useUserPackages = true;
+        #
+        #   home-manager.extraSpecialArgs = specialArgs;
+        #   home-manager.users."${username}" = home-module;
+        # }
+      ];
 
-        home-manager.extraSpecialArgs = specialArgs;
-        home-manager.users."${username}" = home-module;
-      }
-    ];
-    # NixOS and home manager together
-    # modules =
-    #   nixos-modules
-    #   ++ [
-    #     # nixos-generators.nixosModules.all-formats
-    #     # {
-    #     #   # formatConfigs.iso = {config, ...}: {};
-    #     #   formatConfigs.proxmox = {config, ...}: {
-    #     #     # custom proxmox's image name
-    #     #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
-    #     #   };
-    #     # }
-    #     #
-    #     home-manager.nixosModules.home-manager
-    #     {
-    #       home-manager.useGlobalPkgs = true;
-    #       home-manager.useUserPackages = true;
-    #
-    #       home-manager.extraSpecialArgs = specialArgs;
-    #       home-manager.users."${username}" = home-module;
-    #     }
-    #   ];
+    # Home manager as a standalone
+    home-manager = {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+
+      home-manager.extraSpecialArgs = specialArgs;
+      home-manager.users."${username}" = home-module;
+    };
   }
