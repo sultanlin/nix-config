6e9d37023b41dc76842f7ce851fd7fb9812d5205
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-11 02:41:32 +0300

    refactoring neovim plugin overlay and adding neotest-java flake, overlay and neovim plugin

--- overlays/neovim-plugin-overlay.nix
+++ overlays/neovim-plugin-overlay.nix
@@ -1,13 +1,34 @@
 {
-  neotest-java,
-  sentiment-nvim,
+  # neotest-java,
+  # sentiment-nvim,
+  inputs,
   ...
-} @ inputs: final: prev: let
+}: final: prev: let
   mkNvimPlugin = src: pname:
-    prev.pkgs.vimUtils.buildVimPlugin {
+    prev.vimUtils.buildVimPlugin {
       inherit pname src;
     };
 in {
   neotest-java = mkNvimPlugin inputs.neotest-java "neotest-java";
   sentiment-nvim = mkNvimPlugin inputs.sentiment-nvim "sentiment.nvim";
 }
+# } @ inputs: (final: prev:
+#   vimPlugins =
+#     prev.vimPlugins
+#     // { let
+#     mkNvimPlugin = src: pname:
+#     in
+#       own-sentiment-nvim = prev.vimUtils.buildVimPlugin {
+#         name = "sentiment.nvim";
+#         src = sentiment-nvim;
+#       };
+#     };
+#   mkNvimPlugin = src: pname:
+#     prev.pkgs.vimUtils.buildVimPlugin {
+#       inherit pname src;
+#     };
+# # in {
+# #   neotest-java = mkNvimPlugin inputs.neotest-java "neotest-java";
+# #   sentiment-nvim = mkNvimPlugin inputs.sentiment-nvim "sentiment.nvim";
+# # }
+# )
