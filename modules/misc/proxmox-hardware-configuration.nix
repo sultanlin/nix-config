30e79d5387fa01c7b7391fe26e43005e9b4e6aa5
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-23 02:03:57 +0300

    Add proxmox VM settings in modules Added a proxmox hardware configuration file in modules for reuse with motivation from ryan4yin's nix config.

--- modules/misc/proxmox-hardware-configuration.nix
+++ modules/misc/proxmox-hardware-configuration.nix
@@ -1,4 +1,8 @@
-{lib, ...}:
+{
+  lib,
+  modulesPath,
+  ...
+}:
 ##############################################################################
 #
 #  Template for Proxmox's VM, mainly based on:
@@ -6,26 +10,33 @@
 #
 #  the url above is used by `nixos-generator` to generate the Proxmox's VMA image file.
 #
+#  Motivation:  https://github.com/ryan4yin/nix-config/blob/main/modules/nixos/server/proxmox-hardware-configuration.nix
+#
 ##############################################################################
 {
   imports = [
     ./base
     ./base.nix
 
+    (modulesPath + "/profiles/qemu-guest.nix")
     # ./home-manager.nix
     # ./derivations/my-derivations.nix
   ];
 
   boot = {
     # after resize the disk, it will grow partition automatically.
+    # enable = true; # Set it in each VM's hardware configuration manually
     growPartition = true;
     kernelParams = ["console=ttyS0"];
     loader.grub = {
-      device = "/dev/vda";
+      device = "/dev/sda";
 
       # we do not support EFI, so disable it.
       efiSupport = false;
       efiInstallAsRemovable = false;
+
+      configurationLimit = 10; # lib.mkDefault 10;
+      useOSProber = true;
     };
 
     loader.timeout = lib.mkForce 3; # wait for 3 seconds to select the boot entry
@@ -38,6 +49,19 @@
     fsType = "ext4";
   };
   # we do not have a /boot partition, so do not mount it.
+
+  # supported file systems, so we can mount any removable disks with these filesystems
+  boot.supportedFilesystems = [
+    "ext4"
+    "btrfs"
+    "xfs"
+    #"zfs"
+    "ntfs"
+    "fat"
+    "vfat"
+    "exfat"
+    "cifs" # mount windows share
+  ];
 
   # it alse had qemu-guest-agent installed by default.
   services.qemuGuest.enable = lib.mkDefault true;
