1bc49ee0e1ec4d18b60d3bbc0ba50c3bd3baddd3
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-01 07:06:07 +0300

    remove ipv6 in docker vm

--- hosts/docker-vm/nfs-mount.nix
+++ hosts/docker-vm/nfs-mount.nix
@@ -7,10 +7,11 @@
   fileSystems."/home/${username}/truenas/Backup" = {
     device = "truenas:/mnt/SingleHDD/Backup";
     fsType = "nfs";
-    # options = [
-    #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-    #   "nfsvers=4.2,defaults"
-    # ];
+    options = [
+      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+      #   "nfsvers=4.2,defaults"
+      "nfsvers=3.0"
+    ];
   };
   fileSystems."/home/${username}/truenas/PlexMedia" = {
     device = "truenas:/mnt/Plex/PlexStorage";
