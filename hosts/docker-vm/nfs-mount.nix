ec7bf31615ab3852ad786aa78dfc5a171a20c4bd
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-03 04:07:43 +0300

    switch to older boot kernel and switching to nfs 4.2

--- hosts/docker-vm/nfs-mount.nix
+++ hosts/docker-vm/nfs-mount.nix
@@ -9,8 +9,9 @@
     fsType = "nfs";
     options = [
       #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-      #   "nfsvers=4.2,defaults"
-      "nfsvers=3.0"
+      # "nfsvers=4.2,defaults"
+      "nfsvers=4.2"
+      # "nfsvers=3.0"
     ];
   };
   fileSystems."/home/${username}/truenas/PlexMedia" = {
