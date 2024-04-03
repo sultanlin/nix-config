2a4f036dd5c3dfa6e9da4d15dc586e9ff894be23
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-04 00:49:38 +0300

    trying to fix slow NAS speed by removing home manager

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -12,25 +12,25 @@ in
     inherit system specialArgs;
     modules =
       nixos-modules
-     ++ [
-    #   # nixos-generators.nixosModules.all-formats
-    #   # {
-    #   #   # formatConfigs.iso = {config, ...}: {};
-    #   #   formatConfigs.proxmox = {config, ...}: {
-    #   #     # custom proxmox's image name
-    #   #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
-    #   #   };
-    #   # }
-    #   #
-    #
-       # Home manager as a NixOS module
-       home-manager.nixosModules.home-manager
-       {
-         home-manager.useGlobalPkgs = true;
-         home-manager.useUserPackages = true;
-    
-         home-manager.extraSpecialArgs = specialArgs;
-         home-manager.users."${username}" = home-module;
-       }
-     ];
+      ++ [
+        # nixos-generators.nixosModules.all-formats
+        # {
+        #   # formatConfigs.iso = {config, ...}: {};
+        #   formatConfigs.proxmox = {config, ...}: {
+        #     # custom proxmox's image name
+        #     proxmox.qemuConf.name = "${config.networking.hostName}-nixos-${config.system.nixos.label}";
+        #   };
+        # }
+        #
+
+        # Home manager as a NixOS module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users."${username}" = home-module;
+        }
+      ];
   }
