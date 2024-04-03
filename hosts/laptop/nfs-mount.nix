aac382680b6f921a52d2f924f2316577735d520e
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-03 06:11:00 +0300

    adding NAS to my laptop

--- hosts/laptop/nfs-mount.nix
+++ hosts/laptop/nfs-mount.nix
@@ -12,4 +12,31 @@
     #   "nfsvers=4.2,defaults"
     # ];
   };
+  fileSystems."/home/${username}/truenas/PlexMedia" = {
+    device = "truenas:/mnt/Plex/PlexStorage";
+    fsType = "nfs";
+    options = [
+      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+      #   "nfsvers=4.2,defaults"
+      "nfsvers=3.0"
+    ];
+  };
+  fileSystems."/home/${username}/truenas/Proxmox" = {
+    device = "truenas:/mnt/Plex/Prox-Vms";
+    fsType = "nfs";
+    options = [
+      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+      #   "nfsvers=4.2,defaults"
+      "nfsvers=3.0"
+    ];
+  };
+  fileSystems."/home/${username}/truenas/temporary" = {
+    device = "truenas:/mnt/largest-used/temporary-backup";
+    fsType = "nfs";
+    options = [
+      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+      #   "nfsvers=4.2,defaults"
+      "nfsvers=3.0"
+    ];
+  };
 }
