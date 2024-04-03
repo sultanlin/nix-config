583bd60765cb280101b5b71fc8fcb5350235ea4a
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-03 06:35:29 +0300

    trying to fix slow NAS speed

--- hosts/docker-vm/nfs-mount.nix
+++ hosts/docker-vm/nfs-mount.nix
@@ -7,41 +7,41 @@
   fileSystems."/home/${username}/truenas/Backup" = {
     device = "truenas:/mnt/SingleHDD/Backup";
     fsType = "nfs";
-    options = [
-      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-      # "nfsvers=4.2,defaults"
-      "nfsvers=4.2,proto=udp"
-      # "nfsvers=3.0"
-    ];
+    # options = [
+    #   #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+    #   # "nfsvers=4.2,defaults"
+    #   "nfsvers=4.2"
+    #   # "nfsvers=3.0"
+    # ];
   };
   fileSystems."/home/${username}/truenas/PlexMedia" = {
     device = "truenas:/mnt/Plex/PlexStorage";
     fsType = "nfs";
-    options = [
-      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-      #   "nfsvers=4.2,defaults"
-      "nfsvers=4.2,proto=udp"
-      # "nfsvers=3.0"
-    ];
+    # options = [
+    #   #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+    #   #   "nfsvers=4.2,defaults"
+    #   "nfsvers=4.2"
+    #   # "nfsvers=3.0"
+    # ];
   };
   fileSystems."/home/${username}/truenas/Proxmox" = {
     device = "truenas:/mnt/Plex/Prox-Vms";
     fsType = "nfs";
-    options = [
-      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-      #   "nfsvers=4.2,defaults"
-      "nfsvers=4.2,proto=udp"
-      # "nfsvers=3.0"
-    ];
+    # options = [
+    #   #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+    #   #   "nfsvers=4.2,defaults"
+    #   "nfsvers=4.2"
+    #   # "nfsvers=3.0"
+    # ];
   };
   fileSystems."/home/${username}/truenas/temporary" = {
     device = "truenas:/mnt/largest-used/temporary-backup";
     fsType = "nfs";
-    options = [
-      #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
-      #   "nfsvers=4.2,defaults"
-      "nfsvers=4.2,proto=udp"
-      # "nfsvers=3.0"
-    ];
+    # options = [
+    #   #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+    #   #   "nfsvers=4.2,defaults"
+    #   "nfsvers=4.2"
+    #   # "nfsvers=3.0"
+    # ];
   };
 }
