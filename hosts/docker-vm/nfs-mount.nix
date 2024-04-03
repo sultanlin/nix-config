e8a1f1dbbed12c0c48602a08c79f271e4579cc22
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-03 04:38:04 +0300

    trying without static IP to check if it's faster NAS speed

--- hosts/docker-vm/nfs-mount.nix
+++ hosts/docker-vm/nfs-mount.nix
@@ -40,7 +40,8 @@
     options = [
       #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
       #   "nfsvers=4.2,defaults"
-      "nfsvers=3.0"
+      "nfsvers=4.2"
+      # "nfsvers=3.0"
     ];
   };
 }
