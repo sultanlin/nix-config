e78dd915ec2ebb9825fe6e080e237d02ddee8340
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-03 06:39:29 +0300

    trying to fix slow NAS speed

--- modules/base/misc2.nix
+++ modules/base/misc2.nix
@@ -32,7 +32,7 @@
   ];
 
   services = {
-    # gvfs.enable = true; # Mount, trash, and other functionalities
+    gvfs.enable = true; # Mount, trash, and other functionalities
     tumbler.enable = true; # Thumbnail support for images
   };
 
