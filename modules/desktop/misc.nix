d025381adbb324869a19489029d79e0f6e5762a9
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-06-22 21:27:01 +0300

    add postgresql

--- modules/desktop/misc.nix
+++ modules/desktop/misc.nix
@@ -51,4 +51,12 @@
   #   # package = pkgs.mariadb;
   #   package = pkgs.mysql84;
   # };
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = ["mydatabase"];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
+  };
 }
