12693197d7c667da11ef9dbf0533b996a0d538ee
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-02-28 18:20:09 +0300

    feature postgres: add postgres database and related apps

--- modules/desktop/misc.nix
+++ modules/desktop/misc.nix
@@ -56,7 +56,7 @@
     slack
 
     # Database GUI
-    pgadmin4
+    # pgadmin4
     dbeaver-bin
     pgmanage
   ];
@@ -67,22 +67,45 @@
     STEAM_EXTRA_COMPAT_TOOLS_PATH = "/home/user/.steam/root/compatibilitytools.d";
   };
 
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-    # package = pkgs.mysql84;
-  };
+  # services.mysql = {
+  #   enable = true;
+  #   package = pkgs.mariadb;
+  #   # package = pkgs.mysql84;
+  # };
 
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = ["mydatabase"];
-    # settings = {
-    #   ssl = true;
+  services = {
+    postgresql = {
+      enable = true;
+      ensureDatabases = ["mydatabase"];
+      # settings = {
+      #   ssl = true;
+      # };
+      # initialScript = ''
+      #   CREATE ROLE pgadmin WITH PASSWORD 'pgadmin' SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS LOGIN;
+      #   CREATE DATABASE pgadmin;
+      #   GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pgadmin;
+      #   GRANT ALL PRIVILEGES ON DATABASE postgres TO pgadmin;
+      #   GRANT ALL PRIVILEGES ON DATABASE pgadmin TO pgadmin;
+      # '';
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  auth-method
+        local all       pgadmin peer
+        local all       sultan  peer
+        local all       all     trust
+        host  all       all     0.0.0.0/0 scram-sha-256
+      '';
+    };
+    # pgadmin = {
+    #   enable = true;
+    #   initialEmail = "skorpion219@hotmail.co.uk";
+    #   # initialPasswordFile = config.sops.secrets.pg_pass.path;
+    #   openFirewall = true;
+    #   settings = {
+    #     "ALLOWED_HOSTS" = [
+    #       "192.168.0.0/16"
+    #     ];
+    #     "CONFIG_DATABASE_URI" = "postgresql://pgadmin:pgadmin@localhost/pgadmin";
+    #   };
     # };
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-      host  all       all     0.0.0.0/0 scram-sha-256
-    '';
   };
 }
