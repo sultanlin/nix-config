123fefc5ab8b1ea9ae14d3a011158e6bd1dc0ec7
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-04-01 23:10:27 +0300

    fix depracated bat themes option

--- home/base/server/bat.nix
+++ home/base/server/bat.nix
@@ -8,7 +8,12 @@
     };
     themes = {
       # https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme
-      Catppuccin-mocha = builtins.readFile "${catppuccin-bat}/themes/Catppuccin Mocha.tmTheme";
+      # Catppuccin-mocha = builtins.readFile "${catppuccin-bat}/themes/Catppuccin Mocha.tmTheme";
+      # https://www.reddit.com/r/NixOS/comments/1g1xsyk/any_one_knows_how_to_set_up_catppuccinbat_in_nixos/
+      Catppuccin-mocha = {
+        src = catppuccin-bat;
+        file = "extras/Catppuccin\ Mocha.tmTheme";
+      };
     };
   };
 }
