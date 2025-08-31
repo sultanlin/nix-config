34711658b3ee91a30a3adafd2a3bbcd60d5537b7
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-08-31 09:54:25 +0300

    add media nfs mnt

--- hosts/common/nfs-mount.nix
+++ hosts/common/nfs-mount.nix
@@ -34,4 +34,14 @@
     #   # "nfsvers=3.0"
     # ];
   };
+  fileSystems."/home/${username}/truenas/Media" = {
+    device = "truenas:/mnt/hoard/Media";
+    fsType = "nfs";
+    # options = [
+    #   #   # "vers=3.0,uid=1000,gid=100,dir_mode=0755,file_mode=0755,mfsymlinks,credentials=${config.age.secrets.smb-credentials.path},nofail"
+    #   #   "nfsvers=4.2,defaults"
+    #   "nfsvers=4.2"
+    #   # "nfsvers=3.0"
+    # ];
+  };
 }
