996febe54fbc378a2bec682f13d1f067e1af40a8
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-12-21 05:10:41 +0300

    kitty remove blur, fix monolisa font nerd icons, and change font size

--- home/base/server/terminal/kitty.nix
+++ home/base/server/terminal/kitty.nix
@@ -16,7 +16,7 @@
 #   6. And Other common shortcuts such as Copy, Paste, Cursor Move, etc.
 #   7. Search in the current window(show_scrollback): `ctrl + shift + h`
 #          This will open a pager, it's defined by `scrollback_pager`, default is `less`
-#        
+#
 #
 # Useful Hot Keys for Linux:
 #   1. New Tab: `ctrl + shift + t`
@@ -35,21 +35,27 @@
     # and it's installed by home-manager if `theme` is specified.
     themeFile = "Catppuccin-Mocha";
     font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 13;
+      name = "";
+      size = 14;
     };
 
     keybindings = {
       "ctrl+shift+m" = "toggle_maximized";
     };
 
-    settings =
-      {
-        background_opacity = "0.93";
-        macos_option_as_alt = true; # Option key acts as Alt on macOS
-        scrollback_lines = 10000;
-        enable_audio_bell = false;
-        tab_bar_edge = "top";     # tab bar on top
-      };
+    settings = {
+      # background_opacity = "0.95";
+      macos_option_as_alt = true; # Option key acts as Alt on macOS
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      tab_bar_edge = "top"; # tab bar on top
+
+      # Nerd Fonts v3.3.0
+      # monolisa font wasn't working well with nerd font icons/symbols
+      # https://github.com/MonoLisaFont/feedback/issues/53#issuecomment-1336163548
+      # Map the specified unicode codepoints to a particular font.
+      symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
+      # font_features = "MonoLisa +ss02 +ss04 +ss11";
+    };
   };
 }
